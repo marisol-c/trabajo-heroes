23dd527c654a033c0e47aec35a7a1d6d926526f6
marisol-c <marisol-contreras-@hotmail.com>
2021-06-24 17:22:19 -0300

    puerto

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 const express= require('express');
+const app = expres();
+
+
+app.get('/',(req,res)=>{ res.send('hola')});
+
+app.listen(3030, () => console.log("servidor corriendo en el puerto 3030"));
