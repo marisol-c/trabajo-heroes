d9afccbcaef35c53b6a0dab0bc80395dd0448647
marisol-c <marisol-contreras-@hotmail.com>
2021-06-26 00:12:46 -0300

    agregue el final

--- app.js
+++ app.js
@@ -5,17 +5,13 @@ const path = require("path");
 
 app.use(express.static('public'));
 
-app.get('/home',(req,res)=>{res,sendFile(path.join(__dirname,'/views/index.html'))})
+app.get('/',(req,res)=> res.sendFile(path.join(__dirname,'views','index.html')))
 app.get('/babbage',(req,res)=> {res.sendFile(path.join(__dirname,'/views/babbage.html'))});
 app.get('/berners-lee',(req,res)=>{res.sendFile(path.join(__dirname,'/views/berners-lee.html'))});
-app.get('/clarke',(req,res)=>{res,sendFile(path.join(__dirname,'/views/clarke.html'))});
-app.get('/hamiton',(req,res)=>{res,sendFile(path.join(__dirname,'/views/hamiton.html'))});
-app.get('/hopper',(req,res)=>{res,sendFile(path.join(__dirname,'/views/hopper.html'))});
-app.get('/lovelace',(req,res)=>{res,sendFile(path.join(__dirname,'/views/lovelace.html'))});
-app.get('/turin-',(req,res)=>{res,sendFile(path.join(__dirname,'/views,turin-.html'))});
+app.get('/clarke',(req,res)=>{res.sendFile(path.join(__dirname,'/views/clarke.html'))});
+app.get('/hamilton',(req,res)=>{res.sendFile(path.join(__dirname,'/views/hamilton.html'))});
+app.get('/hopper',(req,res)=>{res.sendFile(path.join(__dirname,'/views/hopper.html'))});
+app.get('/lovelace',(req,res)=>{res.sendFile(path.join(__dirname,'/views/lovelace.html'))});
+app.get('/turing-',(req,res)=>{res.sendFile(path.join(__dirname,'/views/turing-.html'))});
 
-
-app.get('/',(req,res)=>{res.send('hola')});
-
-
-app.listen(3030,()=>console.log("servidor corriendo en el puerto 3030"));
+app.listen(port,()=>console.log("servidor corriendo en el puerto 3030"));
